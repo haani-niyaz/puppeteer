9d356d3fba30240f46fd70709f3ffe117632b9ba
Haani Niyaz <haani.niyaz@gmail.com>
2018-08-01 08:48:35 +1000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(name='puppeteer', version='0.1', packages=find_packages(), scripts=['puppeteer/bin/puppeteer'])
