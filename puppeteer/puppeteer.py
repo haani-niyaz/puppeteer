4572f9c90e3bc7a00a0821479a101b7758596a48
Haani Niyaz <haani.niyaz@gmail.com>
2018-08-06 08:31:21 +1000

    Test cli options

--- puppeteer/puppeteer.py
+++ puppeteer/puppeteer.py
@@ -1,16 +1,21 @@
 """Application behaviour"""
 
 from config import YAMLFileReader, YAMLFileReaderError
+import cmd_options
 
 def main():
 
+	cli = cmd_options.main()
+
 	try:
 		user_config = YAMLFileReader().read()
-		print (user_config)
+		# Debug
+		print (user_config)	
 	except YAMLFileReaderError, e:
 		print(e)
 
+	if cli.sub_cmd == 'init':
+		print('do init stuff')
 
-if __name__ == '__main__':
-	pass
-	
+	elif cli.sub_cmd == 'tag':
+		print('do tag stuff')
