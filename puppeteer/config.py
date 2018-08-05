c129924765ca837175ae6b04095f049c83c05dda
Haani Niyaz <haani.niyaz@gmail.com>
2018-08-05 17:35:29 +1000

    Config reader

---
+++ puppeteer/config.py
@@ -0,0 +1,13 @@
+"""Load and validate YAML files"""
+
+import os
+import yaml
+
+class YAMLFileReaderError(Exception):
+	"""An exception that occurs when a config file fails to load"""
+	pass
+
+
+class YAMLFileReader():
+	"""Read YAML files"""
+	pass
--- puppeteer/puppeteer.py
+++ puppeteer/puppeteer.py
@@ -1,2 +1,16 @@
 """Application behaviour"""
 
+from config import YAMLFileReader, YAMLFileReaderError
+
+def main():
+
+	try:
+		user_config = YAMLFileReader()
+		print (user_config)
+	except YAMLFileReaderError, e:
+		print(e)
+
+
+if __name__ == '__main__':
+	pass
+	
