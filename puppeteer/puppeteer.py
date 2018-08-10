e41669503262649146646f102a8f33fbea9602cb
Haani Niyaz <haani.niyaz@gmail.com>
2018-08-10 17:33:29 +1000

    Invoke role tagging

--- puppeteer/puppeteer.py
+++ puppeteer/puppeteer.py
@@ -8,7 +8,7 @@ from controllers.tag import Tag, TagError
 from colourize import color
 
 USER_CONFIG='.puppeteer.yml'
-
+REQUIREMENTS='requirements.yml'
 
 def main():
 
@@ -29,3 +29,35 @@ def main():
 		except BootstrapControllerError, e:
 			print(color('red',e))
 			sys.exit(1)
+
+	elif cli.sub_cmd == 'tag':
+
+		try:
+			req_file = "environments/{0}/{1}".format(cli.env,REQUIREMENTS)
+			content = YAMLFileReader(req_file)
+			repo_list = content.read()
+
+		except YAMLFileReaderError, e:
+			print(color('red',e))
+			print(color('red', "<Puppeteer> : Cannot access '{0}' file.".format(req_file)))
+			sys.exit(1)
+
+		try:
+			tag = Tag(repo_list)
+			updated_repo_list = tag.retag_repo(cli.role, cli.version)
+		except TagError, e:
+			if e.ec == TagError.EXISTS:
+				print(color('yellow',e.message))
+				sys.exit(0)
+			else:
+				print(color('red',e.message))
+				sys.exit(1)
+
+		try:
+			content.write(updated_repo_list)
+			print('Update was successful:')
+			print(color('green', tag.confirm_tag(cli.role)))
+		except YAMLFileReaderError, e:
+			print(color('red',e))
+			sys.exit(1)
+
