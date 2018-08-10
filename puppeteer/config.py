edaf2184399868c34e277859bb86d035b7c2ca3a
Haani Niyaz <haani.niyaz@gmail.com>
2018-08-10 17:36:26 +1000

    Update module and class name to not include verb

--- puppeteer/config.py
+++ puppeteer/config.py
@@ -4,22 +4,22 @@ import os
 import yaml
 
 
-class YAMLFileReaderError(Exception):
+class YAMLFileError(Exception):
 	"""An exception that occurs when a config file fails to load"""
 	pass
 
 
-class YAMLFileReader():
+class YAMLFile():
 	"""Read YAML files"""
 
-	def __init__(self, config_file):
-		self.infile = config_file
+	def __init__(self, path):
+		self.infile = path
 
 
 	def _validate_path(self):
 		
 		if not os.path.exists(self.infile):
-			raise YAMLFileReaderError("User config file '%s' does not exist in current directory." % self.infile )
+			raise YAMLFile("User config file '%s' does not exist in current directory." % self.infile )
 
 
 	def read(self): 
@@ -32,6 +32,11 @@ class YAMLFileReader():
 		except yaml.YAMLError as e:
 			if hasattr(e, 'problem_mark'):
 				mark = e.problem_mark
-				raise YAMLFileReaderError("%s has errors in position in line %s, column %s" % (self.infile,mark.line+1, mark.column+1))
+				raise YAMLFile("%s has errors in position in line %s, column %s" % (self.infile,mark.line+1, mark.column+1))
 			else:
-				raise YAMLFileReaderError("Something went wrong while attempting to read %s" % self.infile )
+				raise YAMLFile("Something went wrong while attempting to read %s" % self.infile )
+
+	def write(self,data):
+
+		with open(self.infile, 'w') as outfile:
+			yaml.dump(data, outfile, default_flow_style=False)			
--- puppeteer/puppeteer.py
+++ puppeteer/puppeteer.py
@@ -1,7 +1,7 @@
 """Application behaviour"""
 
 import sys
-from config import YAMLFileReader, YAMLFileReaderError
+from config import YAMLFile, YAMLFileError
 import cmd_options
 from controllers.bootstrap import BootstrapController, BootstrapControllerError
 from controllers.tag import Tag, TagError
@@ -18,8 +18,8 @@ def main():
 	if cli.sub_cmd == 'init':
 
 		try:
-			user_config = YAMLFileReader(USER_CONFIG).read()
-		except YAMLFileReaderError, e:
+			user_config = YAMLFile(USER_CONFIG).read()
+		except YAMLFileError, e:
 			print(color('red',e))
 			print(color('red', "<Puppeteer> : File must be created if you are running puppeteer for the first time"))
 			sys.exit(1)
@@ -34,10 +34,10 @@ def main():
 
 		try:
 			req_file = "environments/{0}/{1}".format(cli.env,REQUIREMENTS)
-			content = YAMLFileReader(req_file)
+			content = YAMLFile(req_file)
 			repo_list = content.read()
 
-		except YAMLFileReaderError, e:
+		except YAMLFileError, e:
 			print(color('red',e))
 			print(color('red', "<Puppeteer> : Cannot access '{0}' file.".format(req_file)))
 			sys.exit(1)
@@ -57,7 +57,7 @@ def main():
 			content.write(updated_repo_list)
 			print('Update was successful:')
 			print(color('green', tag.confirm_tag(cli.role)))
-		except YAMLFileReaderError, e:
+		except YAMLFileError, e:
 			print(color('red',e))
 			sys.exit(1)
 
