1977a1d88fdc15fa93366885e35102590af3c20d
Haani Niyaz <haani.niyaz@gmail.com>
2018-08-12 18:21:03 +1000

    Use a generic input parameter name

--- puppeteer/controllers/bootstrap.py
+++ puppeteer/controllers/bootstrap.py
@@ -13,9 +13,14 @@ class BootstrapControllerError(Exception):
 class BootstrapController():
 	"""Control repo initialization"""
 
-	def __init__(self, config):
-		self.config = config
-		self.env_dir = 'environments'
+	def __init__(self, data):
+
+		try:
+			self.dirs = data['environments']
+		except KeyError, e:
+			raise BootstrapControllerError('Oops! does your .puppeteer.yml have a list of environments?')
+
+		self.env_dir = 'environments'	
 		self.group_dir = 'group_vars'
 		self.host_dir = 'host_vars'
 		self.repo_file = 'requirements.yml'
@@ -24,12 +29,9 @@ class BootstrapController():
 		"""Create control repo layout"""
 		
 		print(color('cyan','[ ] Initializing environments..'))
-
-		if not self.env_dir in self.config:
-			raise BootstrapControllerError('Oops! does your .puppeteer.yml have a list of environments?')
 	
 		try: 
-			for sub_dir in self.config['environments']:
+			for sub_dir in self.dirs:
 
 				admin_tasks.make_dirs("{0}/{1}/{2}".format(self.env_dir, sub_dir, self.group_dir))
 				admin_tasks.make_dirs("{0}/{1}/{2}".format(self.env_dir, sub_dir, self.host_dir))
