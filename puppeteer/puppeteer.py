a566bcb507486c12405208edb451750c376f4dc3
Haani Niyaz <haani.niyaz@gmail.com>
2018-08-13 14:32:55 +1000

    Fix indentation error

--- puppeteer/puppeteer.py
+++ puppeteer/puppeteer.py
@@ -41,8 +41,8 @@ def run():
   # Setup user config in ansible.cfg
   elif cli.sub_cmd == 'setup':
     print(cli.env)
-      ansible_cfg = AnsibleConfig(user_config, cli.env)
-      ansible_cfg.create_ini()
+    ansible_cfg = AnsibleConfig(user_config, cli.env)
+    ansible_cfg.create_ini()
 
   # Tag a repo with a new version
   elif cli.sub_cmd == 'tag':
