546ae0d71aed4d9cd4bc486ad347dfaae0fea7b3
Haani Niyaz <haani.niyaz@gmail.com>
2018-08-13 14:29:12 +1000

    Refactor and format code

--- puppeteer/puppeteer.py
+++ puppeteer/puppeteer.py
@@ -3,69 +3,74 @@
 import sys
 from config import YAMLFile, YAMLFileError
 import cmd_options
-from controllers.bootstrap import BootstrapController, BootstrapControllerError
+from controllers.bootstrap import Bootstrap, BootstrapError
+from controllers.inigen import AnsibleConfig, AnsibleConfigError
 from controllers.tag import Tag, TagError
 from colourize import color
 from time import sleep
 
-USER_CONFIG='.puppeteer.yml'
-REQUIREMENTS='requirements.yml'
+USER_CONFIG = '.puppeteer.yml'
+REQUIREMENTS = 'requirements.yml'
 
-def main():
 
-	cli = cmd_options.main()
+def run():
 
-	# Initialize repository
-	if cli.sub_cmd == 'init':
+  try:
+    user_config = YAMLFile(USER_CONFIG).read()
+  except YAMLFileError, e:
+    print(color('red', e))
+    print(color('red', ">> File must be created if you are running puppeteer for the first time."))
+    sys.exit(1)
 
-		try:
-			user_config = YAMLFile(USER_CONFIG).read()
-		except YAMLFileError, e:
-			print(color('red',e))
-			print(color('red', "<Puppeteer> : File must be created if you are running puppeteer for the first time"))
-			sys.exit(1)
-		try:
-			
-			bootstrap = BootstrapController(user_config)
-			print(color('cyan','[ ] Initializing environments..'))
-			bootstrap.create_layout()
-			sleep(0.4)
-			print(color('cyan','[x] Done.'))
+  cli = cmd_options.main(user_config['environments'])
 
-		except BootstrapControllerError, e:
-			print(color('red',e))
-			sys.exit(1)
+  # Initialize repository
+  if cli.sub_cmd == 'new':
 
-	elif cli.sub_cmd == 'tag':
+    try:
+      bootstrap = Bootstrap(user_config)
+      print(color('cyan', '>> Initializing environments..'))
+      bootstrap.create_layout()
+      sleep(0.4)
+      print(color('cyan', '>> Done.'))
 
-		try:
-			req_file = "environments/{0}/{1}".format(cli.env,REQUIREMENTS)
-			content = YAMLFile(req_file)
-			repo_list = content.read()
+    except BootstrapError, e:
+      print(color('red', e))
+      sys.exit(1)
 
-		except YAMLFileError, e:
-			print(color('red',e))
-			print(color('red', "<Puppeteer> : Cannot access '{0}' file.".format(req_file)))
-			sys.exit(1)
+  elif cli.sub_cmd == 'tag':
 
-		try:
-			tag = Tag(repo_list)
-			updated_repo_list = tag.retag_repo(cli.role, cli.version)
-		except TagError, e:
-			if e.ec == TagError.EXISTS:
-				print(color('yellow',e.message))
-				sys.exit(0)
-			else:
-				print(color('red',e.message))
-				sys.exit(1)
+    try:
+      req_file = "environments/{0}/{1}".format(cli.env, REQUIREMENTS)
+      content = YAMLFile(req_file)
+      repo_list = content.read()
 
-		try:
-			content.write(updated_repo_list)
-			print(color('cyan','[ ] Updating tag..'))
-			print(color('green', tag.confirm_tag(cli.role)))
-			sleep(0.4)
-			print(color('cyan','[x] Done.'))
-		except YAMLFileError, e:
-			print(color('red',e))
-			sys.exit(1)
+    except YAMLFileError, e:
+      print(color('red', e))
+      print(color('red', ">> Cannot access '{0}' file.".format(req_file)))
+      sys.exit(1)
 
+    try:
+      tag = Tag(repo_list)
+      updated_repo_list = tag.retag_repo(cli.role, cli.version)
+    except TagError, e:
+      if e.ec == TagError.EXISTS:
+        print(color('yellow', e.message))
+        sys.exit(0)
+      else:
+        print(color('red', e.message))
+        sys.exit(1)
+
+    try:
+      content.write(updated_repo_list)
+      print(color('cyan', '>> Updating..'))
+      print(color('green', tag.confirm_tag(cli.role)))
+      sleep(0.4)
+      print(color('cyan', '>> Done.'))
+    except YAMLFileError, e:
+      print(color('red', e))
+      sys.exit(1)
+
+
+if __name__ == '__main__':
+  run()
