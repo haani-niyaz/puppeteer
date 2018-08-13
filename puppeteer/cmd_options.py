7de67fc70e565958933301dda6fd7fc83ddcc256
Haani Niyaz <haani.niyaz@gmail.com>
2018-08-13 14:27:07 +1000

    Refactor command options

--- puppeteer/cmd_options.py
+++ puppeteer/cmd_options.py
@@ -1,20 +1,23 @@
 import argparse
 
 
-def main():
+def main(envs):
 
-	parser = argparse.ArgumentParser(prog='puppeteer',description='Utility to manage ansible workflow')
+  parser = argparse.ArgumentParser(
+      prog='puppeteer', description='Utility to manage ansible workflow')
 
-	subparsers = parser.add_subparsers(dest='sub_cmd',help='sub-command help')
+  subparsers = parser.add_subparsers(dest='sub_cmd', help='sub-command help')
 
-	# Tag parser	
-	parser_tag = subparsers.add_parser('tag', help='a help')
-	parser_tag.add_argument('version', help='version help')
-	parser_tag.add_argument('-r','--role', help='role help')
-	parser_tag.add_argument('-e','--env', help='role help')
+  # Tag parser
+  parser_tag = subparsers.add_parser(
+      'tag', help='tag an environment specific repo with a new version')
+  parser_tag.add_argument('version', help='new version')
+  parser_tag.add_argument('-r', '--role', help='role name')
+  parser_tag.add_argument('-e', '--env', choices=envs,
+                          help='target environment')
 
-	# Init parser
-	parser_init = subparsers.add_parser('init')
+  # Setup new project
+  parser_new = subparsers.add_parser(
+      'new', help='generate layout for a new project')
 
-	return parser.parse_args()
-
+  return parser.parse_args()
