e939cbbc5e5e05369a6d6cc1a91136a2b24e1efc
Haani Niyaz <haani.niyaz@gmail.com>
2018-08-13 14:27:58 +1000

    Setup ansible.cfg configuration

--- puppeteer/cmd_options.py
+++ puppeteer/cmd_options.py
@@ -8,7 +8,7 @@ def main(envs):
 
   subparsers = parser.add_subparsers(dest='sub_cmd', help='sub-command help')
 
-  # Tag parser
+  # Tag operations
   parser_tag = subparsers.add_parser(
       'tag', help='tag an environment specific repo with a new version')
   parser_tag.add_argument('version', help='new version')
@@ -19,5 +19,10 @@ def main(envs):
   # Setup new project
   parser_new = subparsers.add_parser(
       'new', help='generate layout for a new project')
+
+  # Apply user config to ansible.cfg
+  parser_setup = subparsers.add_parser(
+      'setup', help='setup ansible.cfg configuration')
+  parser_setup.add_argument('env', choices=envs, help='target environment')
 
   return parser.parse_args()
