dc24d138e847967976702d3d0fa9bfddcf2e238f
Haani Niyaz <haani.niyaz@gmail.com>
2018-08-13 15:02:52 +1000

    Create inventory file as part of bootstrapping and code format

--- puppeteer/controllers/bootstrap.py
+++ puppeteer/controllers/bootstrap.py
@@ -4,41 +4,48 @@ from utils import admin_tasks
 from colourize import color
 import sys
 
+
 class BootstrapControllerError(Exception):
-	"""An exception that occurs when Bootstrapping a repo"""
-	pass
+  """An exception that occurs when Bootstrapping a repo"""
+  pass
 
 
 class BootstrapController():
-	"""Control repo initialization"""
+  """Control repo initialization"""
 
-	def __init__(self, data):
+  def __init__(self, data):
 
-		try:
-			self.dirs = data['environments']
-		except KeyError, e:
-			raise BootstrapControllerError('Oops! does your .puppeteer.yml have a list of environments?')
+    try:
+      self.dirs = data['environments']
+    except KeyError, e:
+      raise BootstrapControllerError(
+          'Oops! does your .puppeteer.yml have a list of environments?')
 
-		self.env_dir = 'environments'	
-		self.group_dir = 'group_vars'
-		self.host_dir = 'host_vars'
-		self.repo_file = 'requirements.yml'
+    self.env_dir = 'environments'
+    self.group_dir = 'group_vars'
+    self.host_dir = 'host_vars'
+    self.repo_file = 'requirements.yml'
+    self.inventory_file = 'inventory.ini'
 
-	def create_layout(self):
-		"""Create control repo layout"""
-	
-		try: 
-			for sub_dir in self.dirs:
+  def create_layout(self):
+    """Create control repo layout"""
 
-				admin_tasks.make_dirs("{0}/{1}/{2}".format(self.env_dir, sub_dir, self.group_dir))
-				admin_tasks.make_dirs("{0}/{1}/{2}".format(self.env_dir, sub_dir, self.host_dir))
-				admin_tasks.make_file("{0}/{1}/{2}".format(self.env_dir, sub_dir, self.repo_file))
+    try:
+      for sub_dir in self.dirs:
 
-		except admin_tasks.AdminTasksError, e:
-			print(e)
-			sys.exit(1)
+        admin_tasks.make_dirs(
+            "{0}/{1}/{2}".format(self.env_dir, sub_dir, self.group_dir))
+        admin_tasks.make_dirs(
+            "{0}/{1}/{2}".format(self.env_dir, sub_dir, self.host_dir))
+        admin_tasks.make_file(
+            "{0}/{1}/{2}".format(self.env_dir, sub_dir, self.repo_file))
+        admin_tasks.make_file(
+            "{0}/{1}/{2}".format(self.env_dir, sub_dir, self.inventory_file))
 
+    except admin_tasks.AdminTasksError, e:
+      print(e)
+      sys.exit(1)
 
-	def summary(self):
-		"""Bootstrap summary"""
-		pass
+  def summary(self):
+    """Bootstrap summary"""
+    pass
