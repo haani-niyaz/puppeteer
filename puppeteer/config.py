d88c23a2f67c6f25b0d7cd7b95d59ea2a89e59d8
Haani Niyaz <haani.niyaz@gmail.com>
2018-08-13 14:09:31 +1000

    autopep8 formatting

--- puppeteer/config.py
+++ puppeteer/config.py
@@ -5,38 +5,39 @@ import yaml
 
 
 class YAMLFileError(Exception):
-	"""An exception that occurs when a config file fails to load"""
-	pass
+  """An exception that occurs when a config file fails to load"""
+  pass
 
 
 class YAMLFile():
-	"""Read YAML files"""
+  """Read YAML files"""
 
-	def __init__(self, path):
-		self.infile = path
+  def __init__(self, path):
+    self.infile = path
 
+  def _validate_path(self):
 
-	def _validate_path(self):
-		
-		if not os.path.exists(self.infile):
-			raise YAMLFileError("User config file {0} does not exist in current directory.".format(self.infile))
+    if not os.path.exists(self.infile):
+      raise YAMLFileError(
+          ">> User config file {0} does not exist in current directory.".format(self.infile))
 
+  def read(self):
 
-	def read(self): 
+    self._validate_path()
 
-		self._validate_path()
+    try:
+      with open(self.infile, 'r') as stream:
+        return yaml.load(stream)
+    except yaml.YAMLError as e:
+      if hasattr(e, 'problem_mark'):
+        mark = e.problem_mark
+        raise YAMLFile("%s has errors in position in line %s, column %s" % (
+            self.infile, mark.line+1, mark.column+1))
+      else:
+        raise YAMLFile(
+            "Something went wrong while attempting to read %s" % self.infile)
 
-		try:
-			with open(self.infile, 'r') as stream:
-				return yaml.load(stream)
-		except yaml.YAMLError as e:
-			if hasattr(e, 'problem_mark'):
-				mark = e.problem_mark
-				raise YAMLFile("%s has errors in position in line %s, column %s" % (self.infile,mark.line+1, mark.column+1))
-			else:
-				raise YAMLFile("Something went wrong while attempting to read %s" % self.infile )
+  def write(self, data):
 
-	def write(self,data):
-
-		with open(self.infile, 'w') as outfile:
-			yaml.dump(data, outfile, default_flow_style=False)			
+    with open(self.infile, 'w') as outfile:
+      yaml.dump(data, outfile, default_flow_style=False)
