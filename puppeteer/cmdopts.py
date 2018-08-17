97c677c314a13aeff29ff20e66109f8738558a79
Haani Niyaz <haani.niyaz@gmail.com>
2018-08-17 17:37:12 +1000

    Refactor command options

--- puppeteer/cmdopts.py
+++ puppeteer/cmdopts.py
@@ -1,29 +1,40 @@
 import argparse
+import textwrap
 
 
 def main(envs):
 
   parser = argparse.ArgumentParser(
-      prog='puppeteer', description='Utility to manage ansible workflow')
+      prog='puppeteer',
+      description='Utility to manage ansible workflow')
 
   subparsers = parser.add_subparsers(dest='sub_cmd', help='sub-command help')
 
-  # Tag operations
-  parser_tag = subparsers.add_parser(
-      'role', help='role specific actions')
-  parser_tag.add_argument('-t', '--tag',
-                          help='tag a role with a version')
-  parser_tag.add_argument('name', help='name of role')
-  parser_tag.add_argument('-e', '--env', choices=envs,
-                          help='target environment')
+  # Role operations
+  parser_role = subparsers.add_parser(
+      'role',
+      formatter_class=argparse.RawDescriptionHelpFormatter,
+      help='role specific actions',
+      description=textwrap.dedent('''example:
+
+  # Tag role jenkins in dev environment requirements.yml with version 2.0.0
+  puppeteer role jenkins -e dev -t 2.0.0
+      '''))
+  parser_role.add_argument('-t', '--tag',
+                           help='tag a role with a version')
+  parser_role.add_argument('name', help='name of role')
+  parser_role.add_argument('-e', '--env', choices=envs,
+                           help='target environment')
 
   # Setup new project
   parser_new = subparsers.add_parser(
-      'new', help='generate layout for a new project')
+      'new',
+      help='generate layout for a new project')
 
   # Apply user config to ansible.cfg
-  parser_setup = subparsers.add_parser(
-      'gen', help='generate ansible.cfg file')
-  parser_setup.add_argument('env', choices=envs, help='target environment')
+  parser_gen = subparsers.add_parser(
+      'config',
+      help='generate ansible.cfg file')
+  parser_gen.add_argument('env', choices=envs, help='target environment')
 
   return parser
