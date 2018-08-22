6579ccc911f9f8dc3bc93a75d5c94aaddef50ede
Haani Niyaz <haani.niyaz@gmail.com>
2018-08-22 16:35:05 +1000

    Additional helpers

--- puppeteer/utils/admin_tasks.py
+++ puppeteer/utils/admin_tasks.py
@@ -4,28 +4,41 @@
 import os
 import errno
 import pwd
-import subprocess
+from subprocess import check_output, CalledProcessError, STDOUT
+
 
 class AdminTasksError(Exception):
-    """An exception that occurs when performing administrative operations"""
-    pass
+  """An exception that occurs when performing administrative operations"""
+  pass
+
 
 def make_dirs(dirs):
-    """Create directories recursively if it does not exist"""
+  """Create directories recursively if it does not exist"""
 
-    try:
-        os.makedirs(dirs)
-    except OSError, e:
-        if e.errno == errno.EEXIST:
-            return("'{0}' directory already exists".format(dirs))
-        else:
-            raise AdminTasksError(
-                "Backup directory creation failed with error {0}".format(str(e)) )
-
-def  make_file(infile):
-    """Create a file if does not exist"""
-
-    if not os.path.exists(infile):
-        with open(infile, 'w'): pass
+  try:
+    os.makedirs(dirs)
+  except OSError, e:
+    if e.errno == errno.EEXIST:
+      return("'{0}' directory already exists".format(dirs))
     else:
-        return("'{0}' file already exists".format(infile))
+      raise AdminTasksError(
+          "Backup directory creation failed with error {0}".format(str(e)))
+
+
+def make_file(infile):
+  """Create a file if does not exist"""
+
+  if not os.path.exists(infile):
+    with open(infile, 'w'):
+      pass
+  else:
+    return("'{0}' file already exists".format(infile))
+
+
+def run_cmd(cmd):
+
+  try:
+    check_output(
+        cmd, stderr=STDOUT)
+  except CalledProcessError, e:
+    print(e.output)
