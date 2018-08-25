f651b956e3bc37c354ad46a2ac78b96b73f12c28
Haani Niyaz <haani.niyaz@gmail.com>
2018-08-25 20:03:37 +1000

    Format file

--- puppeteer/colourize.py
+++ puppeteer/colourize.py
@@ -1,19 +1,15 @@
 
-COLORS = { 	'cyan': '\033[36m',
-			'pink': '\033[95m', 
-			'blue': '\033[94m', 
-			'green': '\033[92m', 
-			'yellow': '\033[93m', 
-			'red': '\033[91m',
-      		'ENDC': '\033[0m', 
-      		'bold': '\033[1m', 
-      		'underline': '\033[4m'
-      	}
+COLORS = {'cyan': '\033[36m',
+          'pink': '\033[95m',
+          'blue': '\033[94m',
+          'green': '\033[92m',
+          'yellow': '\033[93m',
+          'red': '\033[91m',
+          'ENDC': '\033[0m',
+          'bold': '\033[1m',
+          'underline': '\033[4m'
+          }
 
 
 def color(color, data):
-    return COLORS[color] + str(data) + COLORS['ENDC']
-
-
-if __name__ == '__main__':
-	pass
+  return COLORS[color] + str(data) + COLORS['ENDC']
