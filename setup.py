456f8ede04bc4737b8cb3ea1c2d3277da7e2f7f5
Haani Niyaz <haani.niyaz@gmail.com>
2018-08-25 20:09:09 +1000

    Update setup details

--- setup.py
+++ setup.py
@@ -6,7 +6,10 @@ from setuptools import setup, find_packages
 setup(
     name='puppeteer',
     version='0.1',
-    packages=find_packages(),
+    author='Haani Niyaz',
+    author_email='haani.niyaz@gmail.com',
+    keywords='ansible development productivity workflow',
+    packages=find_packages(exclude=['docs', 'tests']),
     entry_points={
             'console_scripts': [
                 'puppeteer=puppeteer:main',
