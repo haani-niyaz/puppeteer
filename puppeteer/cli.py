252fac15f3139825885188ec671c28d315b2bdfa
Haani Niyaz <haani.niyaz@team.telstra.com>
2019-05-10 10:15:56 +1000

    Remove exit statement if version is already set in loop

--- puppeteer/cli.py
+++ puppeteer/cli.py
@@ -22,22 +22,16 @@ def execute_tag_role(name='', tag='', env=''):
   try:
     role = Role(env)
     updated_repo_data = role.tag(name, tag)
-  except RoleError, e:
-    if e.ec == RoleError.EXISTS:
-      print(color('yellow', e.message + ' in {0}'.format(env)))
-      sys.exit(0)
-    else:
-      print(color('red', "{0} {1}".format(CROSS, e.message)))
-      sys.exit(1)
-
-  try:
     role.update_repo_file(updated_repo_data)
     print(color('cyan', '+ Updating {0} environment..'.format(env)))
     print(color('green', role.confirm_tag(name)))
     print(color('cyan', " {0} Done.".format(TICK)))
-  except YAMLFileError, e:
-    print(color('red', e))
-    sys.exit(1)
+  except RoleError, e:
+    if e.ec == RoleError.EXISTS:
+      print(color('yellow', e.message + ' in {0}'.format(env)))
+    else:
+      print(color('red', "{0} {1}".format(CROSS, e.message)))
+      sys.exit(1)
 
 
 def execute_fetch_roles(env, force):
